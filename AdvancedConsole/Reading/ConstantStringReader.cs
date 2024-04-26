3a41b5447574246302f57b5da253733a46ce3fb6
reosfire <2.com9761721@gmail.com>
2024-04-26 10:02:50 +0300

    ASDzxc

---
+++ AdvancedConsole/Reading/ConstantStringReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using AdvancedConsole.Commands;
+
+namespace AdvancedConsole.Reading
+{
+    public class ConstantStringReader : ICommandReader
+    {
+        private string[] _content;
+        private int _idx;
+        
+        public ConstantStringReader(string content)
+        {
+            _content = content.Split("; ");
+        }
+        public string ReadCommand()
+        {
+            if (_idx >= _content.Length) throw new ReadingCancellationToken();
+            return _content[_idx++];
+        }
+    }
+}
